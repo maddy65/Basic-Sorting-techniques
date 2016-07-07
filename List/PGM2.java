af7369c98dfe43979e77199c4d65aa8d8ca0b624
maddy <maddyraj65@gmail.com>
2016-07-07 21:10:01 +0530

    P

---
+++ List/PGM2.java
@@ -0,0 +1,24 @@
+package TC;
+
+public class PGM2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int c = 0;
+		//for( c =0 ; c<10;c++){
+		for(int i=0;i<10;i++){	
+		System.out.println(c);
+		}*/
+		
+		 
+	    
+	    int row, no;
+		for(row=1;row<=10;row++){
+			for(no=1;no<=row;no++){
+				System.out.println("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ List/PGM3.java
@@ -0,0 +1,18 @@
+package TC;
+import java.util.Scanner;
+
+public class PGM3 {
+
+	public static void main(String[] args) {
+		int n ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input");
+		while((n = sc.nextInt())!=0){
+			System.out.println("you entered "+n);
+			System.out.println("Input Integer");
+		}
+		System.out.println("Out of loop");
+
+	}
+
+}
