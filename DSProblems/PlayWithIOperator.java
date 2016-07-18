1828b109840556ba571a690bf9c51a894b1daf43
maddy <maddyraj65@gmail.com>
2016-07-18 22:46:26 +0530

    Operator test

---
+++ DSProblems/PlayWithIOperator.java
@@ -0,0 +1,18 @@
+package DSProblems;
+
+public class PlayWithIOperator {
+
+	public static void main(String[] args) {
+		int n1 = 2;
+		int n2 = 4;
+		int n3 = n1^n2;
+		System.out.println(n3);
+		n3 = n1&n2;
+		System.out.println(n3);
+		n3 = n1|n2;
+		System.out.println(n3);
+		
+		
+	}
+
+}
