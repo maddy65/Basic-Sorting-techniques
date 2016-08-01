36b2e86c444acc5a9e176ac26d7f68d7b56dbf22
maddy <maddyraj65@gmail.com>
2016-08-01 23:51:50 +0530

    Some Operators

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int x;
+		x = 1;
+		do{
+			System.out.println(x);
+			x++;
+		}while(x<=10);
+	}
+
+}
---
+++ foreach.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		int arr[] = {200,-19,56,44,99};
+		
+		for(int i : arr){
+			System.out.println(i);
+			
+		}	
+	}
+
+}
