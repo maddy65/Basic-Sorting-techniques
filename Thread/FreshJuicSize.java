aca0baea5ad13d016b9c70a83d58f7b3f5321c9d
maddy <maddyraj65@gmail.com>
2016-08-02 20:47:45 +0530

    Examples

---
+++ Thread/FreshJuicSize.java
@@ -0,0 +1,11 @@
+package CoreConcept;
+
+public class FreshJuicSize {
+
+	public static void main(String[] args) {
+		FreshJuice F1 = new FreshJuice();
+		F1.size = FreshJuice.FreshJuicSize.Medium;
+		System.out.println("Size "+F1.size);
+	}
+
+}
---
+++ Thread/FreshJuice.java
@@ -0,0 +1,7 @@
+package CoreConcept;
+
+public class FreshJuice {
+	enum FreshJuicSize {Small, Medium, Large}
+	FreshJuicSize size;
+
+}
