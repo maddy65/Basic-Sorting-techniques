67b47dd38932b819394f6e6f4642ee48e7dd8ef2
maddy <maddyraj65@gmail.com>
2016-08-04 08:52:58 +0530

    Shutdown Hook

---
+++ Thread/Shutdown.java
@@ -0,0 +1,16 @@
+class Shutdown extends Thread{  
+    public void run(){  
+        System.out.println("shut down hook task completed..");  
+    }  
+}  
+  
+public class TestShutdown1{  
+public static void main(String[] args)throws Exception {  
+  
+Runtime r=Runtime.getRuntime();  
+r.addShutdownHook(new MyThread());  
+      
+System.out.println("Now main sleeping... press ctrl+c to exit");  
+try{Thread.sleep(3000);}catch (Exception e) {}  
+}  
+} 
