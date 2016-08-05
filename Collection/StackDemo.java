e40856f625485f00d0f9375dc5dc58536e7ee022
maddy <maddyraj65@gmail.com>
2016-08-05 21:34:02 +0530

    Demo

--- Collection/StackDemo.java
+++ Collection/StackDemo.java
@@ -1,47 +1,46 @@
-package Collections;
-import java.util.*;
-import java.io.*;
+package CoreConcept;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class StackDemo {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		Stack<Integer> St = new Stack<Integer>();
-		int choice=0;
+		Stack<Integer> st = new Stack<Integer>();
+		int choice = 0;
 		int position, element;
-		BufferedReader Br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while(choice<4){
 			System.out.println("Stack Operation");
 			System.out.println("1. Push an element");
 			System.out.println("2. Pop an element");
 			System.out.println("3. Search an element");
 			System.out.println("4. Exit");
-			System.out.println("Your Choice");
-			
-			choice = Integer.parseInt(Br.readLine());
+			System.out.println("Your Choice : ");
+			choice = Integer.parseInt(br.readLine());
 			switch(choice){
-			case 1: System.out.println("Enter element: ");
-					element = Integer.parseInt(Br.readLine());
-			 		St.push(element);
-			 		break;
-			case 2: Integer Obj = St.pop();
-					System.out.println("Pop object is: "+Obj);
+			case 1: System.out.println("Enter Element");
+					element = Integer.parseInt(br.readLine());
+					st.push(element);
 					break;
-			case 3: System.out.println("Which element ?");
-			 		element = Integer.parseInt(Br.readLine());
-			 		position = St.search(element);
-			 		if(position == -1){
-			 			System.out.println("Element not found.");
-			 		}else{
-			 			System.out.println("Element fount at : "+position);
-			 		}
-			 		break;
-			default :
-			return;
+			case 2: Integer Obj = st.pop();
+					System.out.println("popped= "+Obj);
+					break;
+			case 3: System.out.println("Which Element ? ");
+			        element = Integer.parseInt(br.readLine());
+			        position = st.search(element);
+			        if(position == -1)
+			        	System.out.println("Element not found");
+			        else System.out.println("Position: " +position);
+			        break;
+			default: 
+				return;
 			}
-			System.out.println("Stack Contains: "+St);
+			System.out.println("Stack contains: "+st);
 		}
-		
 	}
 
 }
--- Collection/VectorDemo.java
+++ Collection/VectorDemo.java
@@ -1,27 +1,31 @@
-package Collections;
-import java.util.*;
+package CoreConcept;
+
+import java.util.ListIterator;
+import java.util.Vector;
+import java.io.*;
 
 public class VectorDemo {
 
 	public static void main(String[] args) {
-		 Vector<Integer> V = new Vector<Integer>();
-		 int array[] = {3,4,5,6,7,8,9};
-		 for(int i=0;i<array.length;i++){
-			 V.add(array[i]);
-		 }
-		 System.out.println("Vector Element");
-		 for(int j=0;j<V.size();j++){
-			 System.out.println(V.get(j));
-		 }
-		 ListIterator Itr = V.listIterator();
-		 System.out.println("\n Forward Direction");
-		 while(Itr.hasNext())
-			 System.out.println(Itr.next()+"\t");
-		 
-		 System.out.println("\n Backward Direction");
-		 while(Itr.hasPrevious())
-			 System.out.println(Itr.previous()+"\t");
-		 
+		Vector<Integer> V = new Vector<Integer>();
+		int x[] = {20,25,63,87,47,60};
+		for(int i=0; i<x.length; i++){
+			V.add(x[i]);
+		}
+		System.out.println("Vector Elements : ");
+		for(int i=0; i<V.size(); i++){
+			System.out.println(V.get(i));
+		}
+		System.out.println("Element using ListIterator");
+		ListIterator li = V.listIterator();
+		System.out.println("\n In forward direction: ");
+		while(li.hasNext()){
+			System.out.println(li.next()+"\t");
+		}
+		System.out.println("\n In backward direction: ");
+		while(li.hasPrevious()){
+			System.out.println(li.previous()+"\t");
+		}
 	}
 
 }
