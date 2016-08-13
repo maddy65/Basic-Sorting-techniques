220e787c990b1e3ed89a978e3842852c580c05ab
maddy <maddyraj65@gmail.com>
2016-08-13 09:01:11 +0530

    Capitals Pane

---
+++ Collection/CapitalsPanel.java
@@ -0,0 +1,17 @@
+package SwingPgms;
+import java.awt.*;
+import javax.swing.*;
+
+public class CapitalsPanel extends JPanel {
+
+	CapitalsPanel(){
+		JButton b1 = new JButton("Washington");
+		JButton b2 = new JButton("London");
+		JButton b3 = new JButton("Tokyo");
+		add(b1);
+		add(b2);
+		add(b3);
+	}
+	
+
+}
